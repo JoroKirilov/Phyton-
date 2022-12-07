d0891666abd0c6400915ae512c0249a39e3e7304
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-07 19:52:37 +0200

    FIRST WHILE LOOPS

---
+++ Homeworks/loops/while_loops.py
@@ -0,0 +1,11 @@
+# while<condition>
+#     do something
+#     <do something to update condition>
+#     break
+
+counter = 0
+while counter < 5:
+    print(f"Counter value is: {counter}")
+    counter += 1
+else:
+    print("GAME OVER")
