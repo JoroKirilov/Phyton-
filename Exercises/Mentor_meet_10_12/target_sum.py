c6f5d8e8f2ce13485b2edc356d7602b901d50f3b
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-10 15:42:45 +0200

    find world champion

--- Exercises/Mentor_meet_10_12/target_sum.py
+++ Exercises/Mentor_meet_10_12/target_sum.py
@@ -1,20 +1,23 @@
 def find_target(arr, result):
     search_target = result
+    arr.sort()
+    final_result = []
+    count = 0
     for idx in range(len(arr)):
-        search_target -= arr[idx]
+        search_target = result - arr[idx]
         for idx1 in range(len(arr)):
             if idx != idx1:
-                search_target -= arr[idx1]
-                search2 = search_target
-            for idx2 in range(len(arr)):
-                if idx != idx1 and idx != idx2 and idx1 != idx2:
-                    search_target -= arr[idx2]
-                    if search_target == 0:
-                        print("true")
-                    search_target = search2
+                search2 = search_target - arr[idx1]
+                for idx2 in range(len(arr)):
+                    if idx != idx1 and idx != idx2 and idx1 != idx2:
+                        if search2 == arr[idx2]:
+                            final_result.append({arr[idx], arr[idx1], arr[idx2]})
+                            count += 1
+    return final_result
 
 
 
 my_list = [12, 3, 1, 2, -6, 5, -8, 6]
 target = 0
-find_target(my_list, target)
+
+print(find_target(my_list, target))
