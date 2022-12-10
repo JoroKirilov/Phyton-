c9eda137a3c5c88e8663479f5d9868a280fea8f9
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-10 15:01:43 +0200

    find world champion

--- Exercises/Mentor_meet_10_12/task02.py
+++ Exercises/Mentor_meet_10_12/task02.py
@@ -1,11 +1,19 @@
 def game_result(teams, results):
+    winner = ""
+    tournament_result = {}
     for index in range(len(teams)):
         if results[index] == 0:
-            print(teams[index][1])
+            winner = teams[index][1]
         else:
-            print(teams[index][0])
+            winner = teams[index][0]
+        tournament_result.setdefault(winner, 0)  # add element in dict only if there is not exist
+        tournament_result[winner] += 3           # increase value
+    v = list(tournament_result.values())
+    k = list(tournament_result.keys())
+    return k[v.index(max(v))]    # max value in dict
 
 
 competitions = [["Germany", "Japan"], ["Japan", "Spain"], ["Spain", "Germany"]]
 results1 = [0, 0, 1]
-game_result(competitions, results1)
+world_cham = game_result(competitions, results1)
+print(world_cham)
