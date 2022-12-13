c1c97136d9f0be9ef253e0f6fd374dab71453bb8
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-13 14:56:55 +0200

    *task

--- Exercises/Lists/list_advance_manup.py
+++ Exercises/Lists/list_advance_manup.py
@@ -2,8 +2,10 @@ nums = [int(el) for el in input().split()]
 data = input()
 my_list = list()
 while not data == 'end':
-    command, _, fromIndex, _, countIndex = data.split()
+    command, additionalData = data.split()
     if command != 'remove':
+        _, fromIndex, _,countIndex = additionalData.sp
+        command, removeIdx, fromIndex, _, countIndex = data.split()
         first_part = nums[:int(fromIndex)]
         exact_part = nums[int(fromIndex):(int(countIndex) + int(fromIndex))]
         last_part = nums[(int(countIndex) + int(fromIndex)):]
@@ -11,15 +13,9 @@ while not data == 'end':
             exact_part.reverse()
         else:
             exact_part.sort()
-
-        for element in first_part:
-            my_list.append(element)
-        for element in exact_part:
-            my_list.append(element)
-        for element in last_part:
-            my_list.append(element)
+        my_list = first_part + exact_part + last_part
     else:
-        toIndexToRemove = int(command[1])
+        toIndexToRemove = int(removeIdx)
         my_list = nums[toIndexToRemove:]
 
     data = input()
