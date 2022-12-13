4d5b297752d4516d12eea1509cd224f27dad9139
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-13 08:42:02 +0200

    start

---
+++ Exercises/Dictionary/student.py
@@ -0,0 +1,16 @@
+student_info = [element for element in input().split(":")]
+student_book = dict()
+while student_info[0][0].isupper():
+    student_id = student_info[1]
+    student_name = student_info[0]
+    student_courses = student_info[2]
+    student_book.setdefault(student_id, [student_name, student_courses])
+    student_info = [element for element in input().split(":")]
+else:
+    courses = [student_info[0].split("_")]
+    searching_courses = courses[0]
+for key, value in student_book.items():
+    if searching_courses == value[1]:
+        print(f"{value[0]} - {key}")
+
+
--- Exercises/Dictionary/task1.py
+++ Exercises/Dictionary/task1.py
@@ -1,3 +1,7 @@
 my_dict = {"orange": "portocal", "apple": "qbylka"}
 print(my_dict["orange"])
 print(my_dict.get("watermelon", "unknown word"))
+
+my_list = ["plovdiv", "forever"]
+my_list.insert(0, "loko")
+print(my_list)
