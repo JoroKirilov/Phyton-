d5fb07ab12f7fc7706fc4dc1573012213fca9894
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-19 16:25:17 +0200

    add last of lap task

--- Exercises/Sets/party.py
+++ Exercises/Sets/party.py
@@ -10,15 +10,6 @@ while True:
     if living_codes == "END":
         break
     guest_code.discard(living_codes)
-vip_list = []
-normal_guest_list = []
 
-for element in guest_code:
-    if element[0].isdigit():
-        vip_list.append(element)
-for element in guest_code:
-    if not element[0].isdigit():
-        normal_guest_list.append(element)
-print(len(vip_list) + len(normal_guest_list))
-print('\n'.join(sorted(vip_list)))
-print('\n'.join(sorted(normal_guest_list)))
+print(len(guest_code))
+print("\n".join(sorted(guest_code)))
