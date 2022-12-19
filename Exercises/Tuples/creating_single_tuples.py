c1aa5a38a59fe4133600be106d35667767afe75f
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-19 12:01:50 +0200

    some exercises with tuples

--- Exercises/Tuples/creating_single_tuples.py
+++ Exercises/Tuples/creating_single_tuples.py
@@ -2,11 +2,12 @@ def sum_names(name1, name2):
     return name1 + name2, "Qko"
 
 
-names = {"One": 1}
+names = {"One": 1, "Two": 2}
 print(type(names))
 
-a = (names,)
+a = (names, )
 print(type(a))
+print(a)
 
 result = sum_names(5, 6)
 print(result)
