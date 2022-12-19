e2d60bbdc62d19912c1113d73361ec6a343be2f4
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-19 19:00:46 +0200

    just function power number WITH CONST

--- Exercises/19_12_Lecture/power_function.py
+++ Exercises/19_12_Lecture/power_function.py
@@ -1,10 +1,12 @@
-def power(x, y=2):
+POWER = 2
+
+
+def power(x, y=POWER, o=10, /, name="Ivan"):
     result = 1
     while y > 0:
         result *= x
         y -= 1
-    return result
+    return result + o, name
 
 
-
-print(power(3))
+print(power(3, name="print"))
