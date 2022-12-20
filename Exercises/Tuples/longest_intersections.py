ecc9c66fb440946a64ba5323ff94b7295a303b06
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-20 10:11:44 +0200

    interseptions

--- Exercises/Tuples/longest_intersections.py
+++ Exercises/Tuples/longest_intersections.py
@@ -1,21 +1,16 @@
-def gen_s(range_str):
-    start, end = [int(x) for x in range_str.split(",")]
+def gen_seq(range_seq):
+    start, end = [int(x) for x in range_seq.split(',')]
     return set(range(start, end + 1))
 
+longest_set = set()
 
 n = int(input())
-lenght_of_interseptions = 0
-longest_interseption_sequence = []
-for i in range(n):  # 0,3-1,2
-    my_range = input().split('-')
-    set1 = gen_s(my_range[0])
-    set2 = gen_s(my_range[1])
-    set_result = set1.intersection(set2)
-    if len(set_result) > lenght_of_interseptions:
-        lenght_of_interseptions = len(set_result)
-        longest_interseption_sequence = set_result
+for i in range(1, n + 1):
+    data = input().split("-")
+    set1 = gen_seq(data[0])
+    set2 = gen_seq(data[1])
+    set3 = set1.intersection(set2)
+    if len(set3) > len(longest_set):
+        longest_set = set3
 
-
-print(f"Longest intersection is [{', '.join([str(x) for x in longest_interseption_sequence])}] with length {lenght_of_interseptions}")
-
-
+print(f"Longest intersection is [{','.join([str(x) for x in longest_set])}] with length {len(longest_set)}")
