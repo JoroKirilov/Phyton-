db8b2dacc48b0ccdf4613567848da68cf9cc3b22
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-22 11:05:08 +0200

    email task

---
+++ Exercises/Class/task5.py
@@ -0,0 +1,43 @@
+# Create class Email. The __init__ method should receive sender, receiver and a content.
+# It should also have a default set to False attribute called is_sent. The class should have two additional methods:
+# •	send() - sets the is_sent attribute to True
+# •	get_info() - returns the following string: "{sender} says to {receiver}: {content}. Sent: {is_sent}"
+
+# You will receive some information (separated by a single space) until you receive the command "Stop".
+# The first element will be the sender, the second one – the receiver, and the third one – the content.
+# On the final line, you will be given the indices of the sent emails separated by comma and space ", ".
+# Call the send() method for the given indices of emails. For each email, call the get_info() method.
+
+class Email:
+    is_sent = False
+    def __init__(self, sender, receiver, content):
+        self.sender = sender
+        self.receiver = receiver
+        self.content = content
+
+    def send(self):
+        self.is_sent = True
+
+    def get_info(self):
+        return print(f"{self.sender} says to {self.receiver}: {self.content}. Sent: {self.is_sent}")
+
+
+mail = []
+while True:
+    input_data = input()
+    if input_data == "Stop":
+        break
+    sender, receiver, content = input_data.split(" ")
+    email = Email(sender, receiver, content)
+    mail.append(email)
+
+index_input = [int(element) for element in input().split(", ")]
+
+
+for idx in index_input:
+    mail[idx].send()
+
+for mails in mail:
+    mails.get_info()
+
+
