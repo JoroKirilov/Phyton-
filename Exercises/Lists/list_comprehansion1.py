4e45825f949e5168b37b04487dce04f7423bc70a
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-24 05:25:52 +0200

    oneliner

---
+++ Exercises/Lists/list_comprehansion1.py
@@ -0,0 +1,9 @@
+def sum_num(num):
+    return num + 45
+
+numbers = [int(num) for num in input().split()]
+numbers1 = [sum_num(num) for num in numbers if int(num) > 0]
+print(numbers1)
+
+a = 5 if len(numbers) > 5 else 66
+print(a)
