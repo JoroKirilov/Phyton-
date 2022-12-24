55756a0b9eb0eee81924af1ab0a87a9d05ed4acb
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-24 06:50:26 +0200

    slicing

---
+++ Exercises/Lists/list_slicing.py
@@ -0,0 +1,7 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+new_list = my_list[1:10]          # in slicing the rules are the same like range() .
+print(new_list)
+
+for idx in range(len(my_list)):   # from 0 to 9 ( 10 elements ) using index
+    print(idx)
