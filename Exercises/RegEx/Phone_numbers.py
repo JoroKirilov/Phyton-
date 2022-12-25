153c8ba789e5debff1c4ec9547a847b7678d8888
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-25 13:10:24 +0200

    add first reg ex

--- Exercises/RegEx/Phone_numbers.py
+++ Exercises/RegEx/Phone_numbers.py
@@ -1,6 +1,9 @@
 import re
 
 data = input()
-patter = r"[+]359[ -]\d[ -][0-9]{3}[ -][0-9]{4}\b"
-matches = re.findall(patter, data)
-print(matches)
+patter = r"(\+359-2-\d{3}-\d{4}\b)|(\+359 \d [0-9]{3} [0-9]{4}\b)"
+matches = re.finditer(patter, data)
+# for p in matches:
+#     print(p.group())
+phones_list = [p.group() for p in matches]
+print(', '.join(phones_list))
