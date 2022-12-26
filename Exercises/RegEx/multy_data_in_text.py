75fdb214c372690bc6d5d2f4c8f5876cf13140e5
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-26 21:39:44 +0200

    regex master

--- Exercises/RegEx/multy_data_in_text.py
+++ Exercises/RegEx/multy_data_in_text.py
@@ -1,10 +1,17 @@
 import re
 
 line = input()
-while line:
-    pattern = r"(>>)?(?P<product>[A-Za-z]+)(<<)?(?P<price>\d+\.?[\d]+)?!?(?P<quntity>\d+)?"
-    matches = re.finditer(pattern, line)
-    for match in matches:
-        result = match.groupdict()
-        print(f"{result['product']} is {result['price']}")
+list_product = []
+total_cost = 0
+while line != 'Purchase':
+    pattern = r"(^>>)(?P<product>\w+)<<(?P<price>\d+(\.\d+)?)\!(?P<quantity>\d+)"
+    match = re.match(pattern, line)
+    if match:
+        obj = match.groupdict()
+        total_cost += float(obj['price']) * int(obj['quantity'])
+        list_product.append(obj['product'])
     line = input()
+print("Bought furniture:")
+for element in list_product:
+    print(element)
+print(f"Total money spend: {total_cost:.2f}")
