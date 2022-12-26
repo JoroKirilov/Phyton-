55de8ac65a0b9641bbd84b5b67e636070ec0413a
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-26 14:35:25 +0200

    100 % done _validword

--- Exercises/RegEx/words_starts_with_some_character.py
+++ Exercises/RegEx/words_starts_with_some_character.py
@@ -1,7 +1,7 @@
 import re
 text = input()
 list_match = []
-pattern = r"(^|\s?)_(?P<word>[A-Za-z0-9]+)\s?"
+pattern = r"(^|\s)_{1}(?P<word>[A-Za-z0-9]+)($|(?=\s))"
 matches = re.finditer(pattern, text)
 for match in matches:
     list_match.append(match['word'])
