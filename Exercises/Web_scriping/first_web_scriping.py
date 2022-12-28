40944e695e4cf346bba3e7a7f23e54137ef6a622
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-28 08:08:31 +0200

    homework done

--- Exercises/Web_scriping/first_web_scriping.py
+++ Exercises/Web_scriping/first_web_scriping.py
@@ -7,9 +7,10 @@ req = requests.get("http://books.toscrape.com/catalogue/category/books_1/index.h
 soup = BeautifulSoup(req.content, "html.parser")
 content = soup.find_all(class_="product_pod")
 content = str(content)
-pattern = r'(?<=title=")(M|S)[A-Za-z0-9:\s-]{10,20}(?=">)'
+pattern = r'(?<=title=")([M|S](.*?))(?=">)'
 titles_list = re.finditer(pattern, content)
 for title in titles_list:
-    print(title.group())
+    if 10 <= len(title.group()) <= 20:
+        print(title.group())
 
 
