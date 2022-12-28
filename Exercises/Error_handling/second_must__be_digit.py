16fc95fc9d389d5be0f1a08b0972fa6c5aa0d623
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-28 15:40:02 +0200

    error handling

--- Exercises/Error_handling/second_must__be_digit.py
+++ Exercises/Error_handling/second_must__be_digit.py
@@ -6,3 +6,12 @@ while True:
     except ValueError as variable_error:
         print('must be digit:')
 print(text * num)
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(matrix[0][-3])
