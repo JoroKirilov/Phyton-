023f9161cb6ba248ced205b2a3a5e9b6aa0c1b4e
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-28 09:03:00 +0200

    homework done with regex

--- Exercises/Web_scriping/title_from_10_to_20_char_regex.py
+++ Exercises/Web_scriping/title_from_10_to_20_char_regex.py
@@ -8,10 +8,7 @@ soup = BeautifulSoup(req.content, "html.parser")
 content = soup.find_all(class_="product_pod")
 content = str(content)
 
-title_pattern = r'(?<=title=")([M|S](.*?))(?=">)'
+title_pattern = r'(?<=title=")[M|S](.){9,19}(?=">)'
 titles_list = re.finditer(title_pattern, content)
 for title in titles_list:
-    if 10 <= len(title.group()) <= 20:
         print(title.group())
-
-
