8b802d27c6098b10b7f25965f3a5a31d5c9e656f
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-30 09:31:20 +0200

    game exercises in progress tic toe

--- Exercises/game_tictoe/input_data.py
+++ Exercises/game_tictoe/input_data.py
@@ -1,3 +1,4 @@
+
 class NotValidSign(Exception):
     pass
 
@@ -6,7 +7,8 @@ def player_one_choose_sign():
     while True:
         try:
             sign = input(r'Choose between \'X\' and \'O\'')
-            if sign != 'X' and sign != 'x' and sign != 'O' and sign != 'o':
+            sign = sign.upper()
+            if sign != 'X' and sign != 'O':
                 raise NotValidSign
             break
         except NotValidSign:
@@ -21,10 +23,82 @@ def player_two_give_sign(player_one_sign: str):
         return 'X'
 
 
+def create_game_table():
+    return [[' ' for _ in range(3)] for _ in range(3)]
+
+
+def print_table(table):
+    print(f'| {table[0][0]} | {table[0][1]} | {table[0][2]} |')
+    print(f'| {table[1][0]} | {table[1][1]} | {table[1][2]} |')
+    print(f'| {table[2][0]} | {table[2][1]} | {table[2][2]} |')
+
+
+def is_win_horizontal_and_vertical(ma, sign):
+    count = 0
+    for i in range(3):
+        for j in range(3):
+            if ma[i][j] == sign:
+                count += 1
+                if count == 3:
+                    return True
+            else:
+                count = 0
+    for i in range(3):
+        for j in range(3):
+            if ma[j][i] == sign:
+                count += 1
+                if count == 3:
+                    return True
+            else:
+                count = 0
+    return False
+
+
+def is_win_diagonal(ma, sign):
+    count = 0
+    for index in range(3):
+        if ma[index][index] == sign:
+            count += 1
+            if count == 3:
+                return True
+        else:
+            break
+    count = 0
+    for index in range(3):
+        if ma[index][2-index] == sign:
+            count += 1
+            if count == 3:
+                return True
+        else:
+            return False
+
+
+def mark_slot(ma, slot, sign):
+    if slot == 1:
+        ma[0][0] = sign
+    elif slot == 2:
+        ma[0][1] = sign
+    elif slot == 3:
+        ma[0][2] = sign
+    elif slot == 4:
+        ma[1][0] = sign
+    elif slot == 5:
+        ma[1][1] = sign
+    elif slot == 6:
+        ma[1][2] = sign
+    elif slot == 7:
+        ma[2][0] = sign
+    elif slot == 8:
+        ma[2][1] = sign
+    else:
+        ma[2][2] = sign
+
+players_info = {}
+
 player_one = input('Player one name:')
 player_two = input('Player two name:')
-player_one_sign = player_one_choose_sign()
-player_two_sign = player_two_give_sign(player_one_sign)
+players_info[player_one] = player_one_choose_sign()
+players_info[player_two] = player_two_give_sign(players_info[player_one])
 
 
 matrix = [
@@ -35,3 +109,27 @@ matrix = [
 print(f'| {matrix[0][0]} | {matrix[0][1]} | {matrix[0][2]} |')
 print(f'| {matrix[1][0]} | {matrix[1][1]} | {matrix[1][2]} |')
 print(f'| {matrix[2][0]} | {matrix[2][1]} | {matrix[2][2]} |')
+
+
+game_table = create_game_table()
+print()
+print('Start game:')
+print()
+for idx in range(9):
+    if idx % 2 == 0:
+        player_on_turn = player_one
+    else:
+        player_on_turn = player_two
+    print_table(game_table)
+    choose_slot = int(input())
+    mark_slot(game_table, choose_slot, players_info[player_on_turn])
+    is_win = is_win_horizontal_and_vertical(game_table, players_info[player_on_turn])
+    if is_win:
+        print(f"{player_on_turn} win the game")
+        break
+    is_win = is_win_diagonal(game_table,players_info[player_on_turn])
+    if is_win:
+        print(f"{player_on_turn} win the game")
+        break
+
+print_table(game_table)
