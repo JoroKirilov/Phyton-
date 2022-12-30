cb6cc3e28a62b80762480992e2d4b74e4fb0637b
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-30 14:14:14 +0200

    add board

--- Projects/main.py
+++ Projects/main.py
@@ -5,13 +5,27 @@ class Player:
 
 
 def read_players():
-    first_player_name = input(f"Player one name: ")
-    second_player_name = input(f"Player two name: ")
+    first_player_name = input(f"Player one name: ").title()
+    second_player_name = input(f"Player two name: ").title()
 
     while True:
         first_player_sign = input(f'{first_player_name} would you like to play with X or 0 ? ').upper()
         if first_player_sign == 'X' or first_player_sign == 'O':
             break
     second_player_sign = 'O' if first_player_sign == 'X' else 'X'
-    return Player(first_player_name, first_player_sign), \
-           Player(second_player_name, second_player_sign)
+    return Player(first_player_name, first_player_sign), Player(second_player_name, second_player_sign)
+
+
+def print_board_numeration():
+    print('This is the numeration of the board')
+    print("| 1 | 2 | 3 |")
+    print("| 4 | 5 | 6 |")
+    print("| 7 | 8 | 9 |")
+
+
+
+first_player, second_player = read_players()
+
+print_board_numeration()
+
+print(f'{first_player.name} starts first')
