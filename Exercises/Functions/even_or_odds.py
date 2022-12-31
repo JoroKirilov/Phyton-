67a8320daeb8f498a7728e67b69252288eb437c8
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-31 10:22:22 +0200

    everything can be involce in args

--- Exercises/Functions/even_or_odds.py
+++ Exercises/Functions/even_or_odds.py
@@ -1,12 +1,12 @@
-def even_odd(command: str, *args):
+def even_odd(*args):
+    command = args[-1]
     even_nums = []
     odd_nums = []
-    for number in args:
-        if number % 2 == 0:
-            even_nums.append(number)
+    for index in range(len(args) - 1):
+        if args[index] % 2 == 0:
+            even_nums.append(args[index])
         else:
-            odd_nums.append(number)
+            odd_nums.append(args[index])
     return even_nums if command == 'even' else odd_nums
 
 
-print(even_odd("even", 1, 2, 3, 4, 5, 6 ))
