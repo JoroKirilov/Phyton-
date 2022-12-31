d6180918f34f1f3b1c86ccd72a7967ce3e2dbe1f
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-31 09:22:35 +0200

    kwargs ** exercises

---
+++ Exercises/Functions/key_words_argument.py
@@ -0,0 +1,5 @@
+def kwargs_length(**kwargs):
+    counter = 0
+    for el in kwargs:
+        counter += 1
+    return counter
--- Exercises/Functions/negative_vs_positive.py
+++ Exercises/Functions/negative_vs_positive.py
@@ -3,5 +3,5 @@ positive_sum = sum([el for el in input_data if el > 0])
 negative_sum = sum([el for el in input_data if el < 0])
 print(negative_sum)
 print(positive_sum)
-print("The positive are stronger than the positives") if positive_sum > abs(negative_sum) \
+print("The positives are stronger than the negatives") if positive_sum > abs(negative_sum) \
     else print("The negatives are stronger than the positives")
