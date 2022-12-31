7eac4262da5833528887e4b0674e6dfda2a5eca5
Georgi Kirilov <gkirilov1987@abv.bg>
2022-12-31 09:44:50 +0200

    function

---
+++ Exercises/Functions/even_or_odds.py
@@ -0,0 +1,12 @@
+def even_odd(command: str, *args):
+    even_nums = []
+    odd_nums = []
+    for number in args:
+        if number % 2 == 0:
+            even_nums.append(number)
+        else:
+            odd_nums.append(number)
+    return even_nums if command == 'even' else odd_nums
+
+
+print(even_odd(1, 2, 3, 4, 5, 6, "even"))
