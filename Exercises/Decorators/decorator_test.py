6816dd1e436b05285936f82ab2d46f259922dcea
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-03 20:13:20 +0200

    decorator workshop

--- Exercises/Decorators/decorator_test.py
+++ Exercises/Decorators/decorator_test.py
@@ -7,19 +7,69 @@ from pathlib import Path
 BASE_DIR = Path(os.path.abspath(__file__)).parent
 
 
+class InvalidFileExtension(Exception):
+    pass
+
+
 def test_file_generator(file_name="decorator_test.csv", min_rows=32, max_rows=64, min_cols=5, max_cols=15):
     with open(os.path.join(BASE_DIR, file_name), "w") as csv_output:
         writer = csv.writer(csv_output)
 
         number_of_rows = random.randint(min_rows, max_rows)
-
         for _ in range(number_of_rows):
             number_of_columns = random.randint(min_cols, max_cols)
             row_data = [
-                random.choice(string.ascii_letters) * 5 for _ in range(number_of_columns)
+                "".join(
+                    [random.choice(string.ascii_letters) for _ in range(5)]
+                )
+                for _ in range(number_of_columns)
             ]
             writer.writerow(row_data)
 
 
-test_file_generator()
+# test_file_generator()
 
+# Make decorator that returns the min length rows of CSV file
+def track_min_length_rows(func):
+    def wrapper_func(file_path):
+        csv_content = func(file_path)
+        shortest_row = min((len(row) for row in csv_content), default=0)
+        shortest_rows = [i for i, row in enumerate(csv_content) if len(row) == shortest_row]
+        print(f"Shortest rows of {file_path.name} are {shortest_rows}")
+        return csv_content
+    return wrapper_func
+
+
+# Make decorator that return the max length rows of CSV file
+def track_max_length_row(func):
+    def wrapper_func(file_path):
+        csv_content = func(file_path)
+        longest_row = max((len(row) for row in csv_content), default=0)
+        longest_rows = [i for i, row in enumerate(csv_content) if len(row) == longest_row]
+        print(f"Longest rows of {file_path.name} are {longest_rows}")
+        return csv_content
+    return wrapper_func
+
+@track_max_length_row
+@track_min_length_rows
+def get_csv_file_content(file_path: Path):
+    # todo Check if file_path is of CSV FILE
+
+    if file_path.suffix != ".csv":
+        print("Not sent correct type")
+        return False
+
+    # Read the CSV file
+    try:
+        with open(file_path, "r") as csv_source:
+            csv_content = csv.reader(csv_source)
+            return list(csv_content)
+
+    except FileNotFoundError:
+        print("No such file or directory")
+        return False
+
+
+file_name = "decorator_test.csv"
+file_path_1 = Path(os.path.join(BASE_DIR, file_name))
+print(get_csv_file_content(file_path_1))
---
+++ Exercises/Decorators/decorator_test1.py
@@ -0,0 +1,102 @@
+import os.path
+import random
+import string
+from pathlib import Path
+from functools import lru_cache
+import csv
+
+BASE_DIR = Path(os.path.abspath(__file__)).parent
+
+
+def print_list(list):
+    row_index = 0
+    for row in list:
+        print(row_index, row)
+        row_index += 1
+
+
+def test_file_generator(file_name="decorator_test.csv", min_rows=32, max_rows=64, min_cols=5, max_cols=15):
+    with open(os.path.join(BASE_DIR, file_name), "w") as csv_output:
+        writer = csv.writer(csv_output)
+
+        number_of_rows = random.randint(min_rows, max_rows)
+        for _ in range(number_of_rows):
+            number_of_columns = random.randint(min_cols, max_cols)
+            row_data = [
+                "".join(
+                    [random.choice(string.ascii_letters) for _ in range(5)]
+                )
+                for _ in range(number_of_columns)
+            ]
+            writer.writerow(row_data)
+
+
+test_file_generator()
+
+
+def track_max_length_rows_of_a_csv_file(func):
+    def wrapper_func(file_path):
+        csv_content = func(file_path)
+        longest_row = max(len(row) for row in csv_content)
+        longest_row = [i for i, row in enumerate(csv_content) if len(row) == longest_row]
+        print(f"Longest rows of {file_path.name} are: {longest_row}")
+        return csv_content
+
+    return wrapper_func
+
+
+def track_min_length_rows_of_a_csv_file(func):
+    def wrapper_func(file_path):
+        csv_content = func(file_path)
+        shortest_row = min(len(row) for row in csv_content)
+        shortest_row = [i for i, row in enumerate(csv_content) if len(row) == shortest_row]
+        print(f"Shortest rows of {file_path.name} are: {shortest_row}")
+        return csv_content
+
+    return wrapper_func
+
+
+# How to make decorator, that returns the min length rows of a CSV file
+# How to make decorator, that returns the max length rows of a CSV file
+# How to make decorator that returns the word with max occurrences ONLY if those are wanted
+
+# Should protect our decorators against not existing files
+
+
+def ensure_csv_content(func):
+    def wrapper_func(file_path):
+        content = func(file_path)
+        if not isinstance(content, list):
+            print("CSV file doesn't have any valid content")
+            return [""]
+        return content
+
+    return wrapper_func
+
+
+@track_min_length_rows_of_a_csv_file
+@track_max_length_rows_of_a_csv_file
+@ensure_csv_content
+def get_csv_file_content(file_path: Path):
+    # Check if file_path is of CSV file, if not exit with Message
+
+    if file_path.suffix != ".csv":
+        print("Not sent correct type of file")
+        return False
+    #
+    # if not Path(file_path).exists():
+    #     return "File not exist"
+
+    # Read the CSV file , using the file_path and return its content as list of lists
+    try:
+        with open(file_path, "r") as csv_source:
+            csv_content = csv.reader(csv_source)
+            return list(csv_content)
+    except FileNotFoundError:
+        print("Given file_path not found on this machine")
+        return False
+
+
+FILE_NAME = "decorators_test.csv"
+file_path_with_path_class = Path(os.path.join(BASE_DIR, FILE_NAME))
+print_list(get_csv_file_content(file_path_with_path_class))
