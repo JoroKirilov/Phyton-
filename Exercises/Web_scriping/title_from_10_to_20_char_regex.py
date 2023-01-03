c9aa701bc306c2223ef5934f319e1b130361e2b8
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-03 14:17:31 +0200

    web scrapling all urls

--- Exercises/Web_scriping/title_from_10_to_20_char_regex.py
+++ Exercises/Web_scriping/title_from_10_to_20_char_regex.py
@@ -2,13 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-req = requests.get("http://books.toscrape.com/catalogue/category/books_1/index.html")
+first_part = "http://books.toscrape.com/catalogue/page-"
+second_part = ".html"
+for index in range(1, 51):
+    searching_url = first_part + str(index) + second_part
+    req = requests.get(searching_url)
+    soup = BeautifulSoup(req.content, "html.parser")
+    content = soup.find_all(class_="product_pod")
+    content = str(content)
 
-soup = BeautifulSoup(req.content, "html.parser")
-content = soup.find_all(class_="product_pod")
-content = str(content)
 
-title_pattern = r'(?<=title=")[M|S](.){9,19}(?=">)'
-titles_list = re.finditer(title_pattern, content)
-for title in titles_list:
+    title_pattern = r'(?<=title=")[M|S](.){9,19}(?=">)'
+    titles_list = re.finditer(title_pattern, content)
+    for title in titles_list:
         print(title.group())
