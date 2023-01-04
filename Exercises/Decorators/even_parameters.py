50c8496754cd63055570a76392bf80fd52ff6e7a
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-04 13:27:26 +0200

    decorator exercises

---
+++ Exercises/Decorators/even_parameters.py
@@ -0,0 +1,22 @@
+# 6
+# Please use only even numbers!
+
+
+
+
+
+
+def even_parameters(func):
+    def wrapper(*args):
+        for arg in args:
+            if not isinstance(arg, int) or arg % 2 != 0:
+                return "Please use only even numbers!"
+        return func(*args)
+    return wrapper
+
+@even_parameters
+def add(a, b):
+    return a + b
+
+print(add("2", 4))
+print(add("Peter", "1"))
