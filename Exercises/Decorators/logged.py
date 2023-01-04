f62aa525e2397e51e52cb32b941442daf56b7da1
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-04 10:17:07 +0200

    decorator exercises

---
+++ Exercises/Decorators/logged.py
@@ -0,0 +1,11 @@
+def logged(function):
+    def wrapper(*args):
+        arguments = "".join(str(args))
+        text_result = f"you called {function.__name__}{arguments}"
+        result = function(*args)
+        return text_result + f"\nit returned {result}"
+    return wrapper
+@logged
+def sum_func(a, b):
+    return a + b
+print(sum_func(1, 4))
