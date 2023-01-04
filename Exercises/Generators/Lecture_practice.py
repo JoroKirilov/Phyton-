1fe901f9135fc41671c50da8c7fa517aada4a9ac
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-04 19:06:11 +0200

    homework done

--- Exercises/Generators/Lecture_practice.py
+++ Exercises/Generators/Lecture_practice.py
@@ -4,7 +4,7 @@ import random
 
 def generate_students_info():
     for e in range(1, 11):
-        for s in range(1, 28000):
+        for s in range(1, 200000):
             yield f"Student {s}", {
                 "student_name": f"Student {s}",
                 "exam": e,
@@ -21,7 +21,7 @@ def generate_students_info_list():
                 "points": random.randint(random.choice([30, 40, 50]), 100),
             }
         )
-        for s in range(1, 28000)
+        for s in range(1, 200000)
         for e in range(1, 11)
     ]
 
@@ -29,7 +29,7 @@ def generate_students_info_list():
 now = datetime.now()
 print(now)
 
-for student_name, student_info in generate_students_info_list():
+for student_name, student_info in generate_students_info():
     print(student_name, student_info)
 
 after_generator_loop = datetime.now()
