731924e4878dc203e244b340e605b104af08f080
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-05 07:13:37 +0200

    time time()

---
+++ Exercises/Decorators/exact_time.py
@@ -0,0 +1,23 @@
+import time
+
+
+def exec_time(func):
+    def wrapper(*args):
+        # start watch
+        start = time.time()
+        func(*args)
+        end = time.time()
+        # end watch
+        return end - start
+        # return seconds
+    return wrapper
+
+
+@exec_time
+def loop(start, end):
+    total = 0
+    for x in range(start, end):
+        total += x
+    return total
+
+print(loop(1, 100000000))
