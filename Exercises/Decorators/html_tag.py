0aff73dc8a6f7cb0d70d11ea22b776ae51b0a5bb
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-05 06:49:09 +0200

    decorator morning exercises

---
+++ Exercises/Decorators/html_tag.py
@@ -0,0 +1,15 @@
+def tags(tag_name):
+    def decorator(func):
+        def wrapper(*args):
+            func_result = func(*args)
+            return f"<{tag_name}>{func_result}</{tag_name}>"
+        return wrapper
+    return decorator
+
+
+@tags('p')
+def join_string(*args):
+    return "".join(args)
+
+
+print(join_string("Hello," " you!"))
