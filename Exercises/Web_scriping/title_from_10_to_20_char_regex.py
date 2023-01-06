3e0f3f37c5325d27efec831d2d7f2bea2e46a577
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-06 15:29:01 +0200

    web scrapling all urls

--- Exercises/Web_scriping/title_from_10_to_20_char_regex.py
+++ Exercises/Web_scriping/title_from_10_to_20_char_regex.py
@@ -10,8 +10,6 @@ for index in range(1, 51):
     soup = BeautifulSoup(req.content, "html.parser")
     content = soup.find_all(class_="product_pod")
     content = str(content)
-
-
     title_pattern = r'(?<=title=")[M|S](.){9,19}(?=">)'
     titles_list = re.finditer(title_pattern, content)
     for title in titles_list:
