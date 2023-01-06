0b25cf5583070b752995762cffface0713fac700
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-06 10:31:16 +0200

    last decorator task done

---
+++ Homeworks/Decorators/03.decorator.py
@@ -0,0 +1,14 @@
+# 3. Write a decorator that prints out "Mary Spring" whenever the decorated function is called.
+def some_decorator(is_call=False):
+    def decorator(func_ref):
+        def wrapper():
+            return "Mary Spring" if is_call else func_ref()
+        return wrapper
+    return decorator
+
+
+@some_decorator(is_call=True)
+def some_function():
+    return "Mary Winter"
+
+print(some_function())
