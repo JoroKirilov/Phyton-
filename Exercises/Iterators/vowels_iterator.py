dda0cf08d2f3beb691543387c4ac0cfc08bdf758
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-07 16:21:19 +0200

    iterator exercises

--- Exercises/Iterators/vowels_iterator.py
+++ Exercises/Iterators/vowels_iterator.py
@@ -1,23 +1,23 @@
-# class Vowels:
-#     def __init__(self, obj):
-#         self.obj = obj
-#         self.i = 0
-#         self.end = len(obj) - 1
-#
-#     def __iter__(self):
-#         return self
-#
-#     def __next__(self):
-#         while self.i <= self.end:
-#             current = self.obj[self.i]
-#             self.i += 1
-#             if current in "AaEeOoIi":
-#                 return current
-#         else:
-#             raise StopIteration
-#
-#
-# str_list = "AvdpeaioiesaciWioreewmk"
-# vowel_iterator = Vowels(str_list)
-# for char in vowel_iterator:
-#     print(char, end=' ')
+class vowels:
+    def __init__(self, obj):
+        self.obj = obj
+        self.i = 0
+        self.end = len(obj) - 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.i <= self.end:
+            current = self.obj[self.i]
+            self.i += 1
+            if current in "AaEeOoIi":
+                return current
+        else:
+            raise StopIteration
+
+
+str_list = "AvdpeaioiesaciWioreewmk"
+vowel_iterator = Vowels(str_list)
+for char in vowel_iterator:
+    print(char, end=' ')
---
+++ Exercises/Iterators/without_iterator_same_func.py
@@ -0,0 +1,4 @@
+my_string = "AoifswWOPFMEOasam"
+for i in my_string:
+    if i in "AaOoEeIi":
+        print(i)
