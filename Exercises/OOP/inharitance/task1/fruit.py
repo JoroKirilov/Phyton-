cf6c976a6aab4c8644d5ec0d1e17c8b39b415af5
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-13 11:46:44 +0200

    Delete Exercises/OOP/inharitance directory

--- Exercises/OOP/inharitance/task1/fruit.py
+++
@@ -1,9 +0,0 @@
-from task1.food import Food
-
-
-class Fruit(Food):
-    def __init__(self, name, expiration_data):
-        super().__init__(expiration_data)
-        self.name = name
-
-
--- Exercises/OOP/inharitance/task2/main.py
+++
@@ -1,5 +0,0 @@
-from project import animal
-from project import dog
-
-dog1 = dog.Dog()
-print(dog1.eat())
