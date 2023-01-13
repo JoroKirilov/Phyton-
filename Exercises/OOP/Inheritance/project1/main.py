684dfede677881598ff726a25f6165d158b0055e
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-13 12:00:55 +0200

    pokemon task

---
+++ Exercises/OOP/Inheritance/project1/main.py
@@ -0,0 +1,10 @@
+from Inheritance.project1.child import Child
+from Inheritance.project1.person import Person
+
+person1 = Person("Peter", 25)
+child1 = Child("Peter Junior", 5)
+
+print(person1.name)
+print(person1.age)
+print(child1.__class__.__bases__[0].__name__)
+print(child1.name)
