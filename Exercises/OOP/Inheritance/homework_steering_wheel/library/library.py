0c5011386a527cb1e7cad9dff687e62b730f3f6f
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-14 10:11:00 +0200

    library registration left

--- Exercises/OOP/Inheritance/homework_steering_wheel/library/library.py
+++ Exercises/OOP/Inheritance/homework_steering_wheel/library/library.py
@@ -11,12 +11,28 @@ class Library:
         if author in self.books_available:
             if book_name in self.books_available[author]:
                 user.take_book(book_name)
-                self.rented_books[user.username] = {book_name: days_to_return}
+                if user.username in self.rented_books:
+                    self.rented_books[user.username].update({book_name:days_to_return})
+                else:
+                    self.rented_books = {user.username: {book_name:days_to_return}}
                 del self.books_available[book_name]
                 return f"{book_name} successfully rented for the next {days_to_return} days!"
         return f"The book {book_name} is already rented and will be available in {days_to_return} days rented by {user.username}!"
 
     def return_book(self, author: str, book_name: str, user: User):
         if book_name in user.books_rent:
-            self.books_available = {author: []}
-            self.books_available[author] =
+            if author not in self.books_available:
+                self.books_available = {author: []}
+                self.books_available[author].append(book_name)
+            else:
+                self.books_available[author].append(book_name)
+
+            for key, value in self.rented_books.items():
+                if book_name in value.keys():
+                    del self.rented_books[key][book_name]
+
+            user.books_rent.remove(book_name)
+
+
+
+
--- Exercises/OOP/Inheritance/test.py
+++ Exercises/OOP/Inheritance/test.py
@@ -1,3 +1,10 @@
 dict1 = {}
-dict1["Gosho"] = {"go": 34}
+dict1 = {"autor": {"iv0": 45}}
+dict1["autor"].update({"gosho": 4})
+dict1["autor"].update({"ivan" : 3})
+
+for key, value in dict1.items():
+    if "gosho" in value.keys():
+        del dict1[key]["gosho"]
+    print("true")
 print(dict1)
