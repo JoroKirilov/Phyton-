86afb4f15101802eba2055105db3e7618a97ad1c
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-19 16:54:59 +0200

    getter master

--- Exercises/OOP/encapsulation/profile.py
+++ Exercises/OOP/encapsulation/profile.py
@@ -28,7 +28,11 @@ class Profile:
         else:
             raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
 
+    def __str__(self):
+        return fr'You have a profile with username: "{self.username}" and password: {"*"*len(self.__password)}'
+
 
 p = Profile("Georgi", "909dAADSDA")
 print(p.username)
 print(p.password)
+print(p)
---
+++ Exercises/OOP/encapsulation/task3.py
@@ -0,0 +1,21 @@
+class Tv:
+    def __init__(self, model: str, password: str):
+        self.model = model
+        self.password = password
+        self.is_on = False
+
+    def turn_on_button(self):
+        self.is_on = not self.is_on
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        self.__password = value
+
+
+lg = Tv("c1", "cinemaDream")
+lg.password = "gamingDream"
+print(lg.password)
