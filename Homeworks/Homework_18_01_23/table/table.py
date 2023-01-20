b450ebcda20360fff04e4948a9d4c7688036c55c
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-20 15:15:08 +0200

    Homework

--- Homeworks/Homework_18_01_23/table/table.py
+++ Homeworks/Homework_18_01_23/table/table.py
@@ -1,6 +1,7 @@
 # Table (seats, material, color) Make a Table class, representing a physical table.
 # It is up to you to choose attributes. The table should allow People to sit on free chairs,
 # where free chairs must be no more than Table's places.
+
 # We should be able to allow People to sit on a free chair and
 # to leave a chair. We should be able to tell what is the free capacity of the Table.
 
@@ -12,10 +13,12 @@ class Table:
         self.free_seats = self.capacity
 
     def get_free_seat(self):
-        return self.free_seats
+        if self.free_seats > self.capacity:
+            return f"There is {self.free_seats} free chairs"
+        return "There are no free chairs"
 
     def sit_on_table(self):
-        if self.get_free_seat():
+        if self.free_seats > 0:
             self.free_seats -= 1
             return "Someone is sitting on the table"
         return "No free chairs"
@@ -30,4 +33,10 @@ table1 = Table("WOOD", "BLACK")
 print(table1.free_seats)
 print(table1.sit_on_table())
 print(table1.sit_on_table())
+print(table1.sit_on_table())
+print(table1.sit_on_table())
+print(table1.sit_on_table())
+print(table1.sit_on_table())
 print(table1.get_up_of_table())
+print(table1.sit_on_table())
+print(table1.sit_on_table())
