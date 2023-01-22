472690f03f6626601b1eb792618fe1516c608cd7
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-22 10:20:06 +0200

    itertools exercises

--- Exercises/Itertools/task1.py
+++ Exercises/Itertools/task1.py
@@ -9,7 +9,37 @@ import itertools
 # daily_data = list(itertools.zip_longest(range(10), data))
 # print(daily_data)
 
-mode = itertools.cycle(["ON", "OFF"])
-print(next(mode))
-print(next(mode))
-print(next(mode))
+# mode = itertools.cycle(["ON", "OFF"])
+# print(next(mode))
+# print(next(mode))
+# print(next(mode))
+#
+# repeat_time = itertools.repeat(2, times=3)
+#
+# print(next(repeat_time))
+# print(next(repeat_time))
+# print(next(repeat_time))
+
+
+#-------------- toto 6/49 all combinations -----------
+
+# list_toto = [i for i in range(1, 50)]
+# print(list_toto)
+# comb = itertools.permutations(list_toto, 6)
+# for com in comb:
+#     print(com)
+
+#------------------------------------------------------
+
+
+# list_comb = [1, 2, 3]
+# result = itertools.combinations(list_comb, 2)
+# for i in result:
+#     print(i)
+
+#-------------------------------------------------------
+
+# list_comb = [1,2,3]
+# result = itertools.permutations(list_comb, 3)
+# for i in result:
+#     print(i)
