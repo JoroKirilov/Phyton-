f9d196083afe7e0cf32d398527a4fad7066df467
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-22 11:53:43 +0200

    itertools exercises advance

--- Homeworks/mentor_meet_21_01/task2.py
+++ Homeworks/mentor_meet_21_01/task2.py
@@ -1,9 +1,11 @@
 import itertools
 
 
-def groupAnagrams(words):
-    return [list(a) for _, a in itertools.groupby(sorted(words, key=lambda a: sorted(a)), key=lambda a: sorted(a))]
-
-
+# def groupAnagrams(words):
+#     return [list(a) for _, a in itertools.groupby(sorted(words, key=lambda a: sorted(a)), key=lambda a: sorted(a))]
+#
+#
 example_input = ['yo', 'act', 'flop', 'tac', 'foo', 'cat', 'oy', 'olfp']
-print(groupAnagrams(example_input))
+# print(groupAnagrams(example_input))
+
+
