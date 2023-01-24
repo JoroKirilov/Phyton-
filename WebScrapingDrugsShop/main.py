448798471afccaa04acf2fd8e67fccaa8d404165
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-24 14:09:59 +0200

    start new project web scraping with learning purposes

--- WebScrapingDrugsShop/main.py
+++ WebScrapingDrugsShop/main.py
@@ -2,19 +2,39 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+def add_to_dict(medic):
+    temp_dict = {}
+    for el in medic:
+        data_element = el.text
+        name_medicine = data_element.split("MRP")
+        temp_dict[name_medicine[0]] = name_medicine[1]
+    return temp_dict
+
+drugs = {}
+
 url = "https://www.1mg.com/drugs-all-medicines"
 headers = {
     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47"
 }
 res = requests.get(url, headers=headers)
-
 soup = BeautifulSoup(res.content, "html.parser")
-drugs = {}
+max_page = soup.findAll(class_="button-text link-page")
+for page in range(2, int(max_page[-1].text) + 1):
+    url = "https://www.1mg.com/drugs-all-medicines?page="+str(page)
+    req = requests.get(url, headers=headers)
+    soup = BeautifulSoup(req.content, "html.parser")
+    medicine = soup.findAll(class_="style__font-bold___1k9Dl style__font-14px___YZZrf style__flex-row___2AKyf style__space-between___2mbvn style__padding-bottom-5px___2NrDR")
+    drugs.update(add_to_dict(medicine))
+
+print(drugs)
+
+
+
+# https://www.1mg.com/drugs-all-medicines?page=2
 medic = soup.findAll(class_="style__font-bold___1k9Dl style__font-14px___YZZrf style__flex-row___2AKyf style__space-between___2mbvn style__padding-bottom-5px___2NrDR")
-for el in medic:
-    data_element = el.text
-    name_medicine = data_element.split("MRP")
-    drugs[name_medicine[0]] = name_medicine[1]
+
 print(drugs["Augmentin 625 Duo Tablet"])
-page = soup.findAll(class_="button-text link-page")
-print(page[-1].text)
+
+print(chr(97))
+
+# https://www.1mg.com/drugs-all-medicines?page=2&label=b
--- book_scraper/main.py
+++ book_scraper/main.py
@@ -1,19 +1,44 @@
 import argparse
 from bs4 import BeautifulSoup
 import requests
+import re
 
-req = requests.get("http://books.toscrape.com/catalogue/category/books_1/index.html")
+req = requests.get("http://books.toscrape.com/catalogue/category/books_1/page-2.html")
 soup = BeautifulSoup(req.content, "html.parser")
-content = soup.find_all(class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
-for cont in content:
-    print(cont.text)
+content = soup.find_all(class_="current")
+pages = content[0].text.split()
+max_page = pages[-1]
+
+book_dict = {}
+
+for page in range(2, int(max_page)):
+    url = "http://books.toscrape.com/catalogue/category/books_1/page-"+str(page)+".html"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, "html.parser")
+    content = soup.find_all(class_="product_pod")
+    content = str(content)
+
+    pattern_title = r"(?<=title=)(.*?)(?=>)"
+    titles_list = re.finditer(pattern_title, content)
+    pattern_price = r'(?<=price_color">)(.*?)(?=</p>)'
+    price_list = re.finditer(pattern_price, content)
+    titles = []
+    prices = []
+    for element in titles_list:
+        titles.append(element.group())
+    for element in price_list:
+        prices.append(element.group())
+    for idx in range(len(titles)):
+        book_dict[titles[idx]] = prices[idx]
+
+if "book_price":
+    print()
 
 
 
-
-
-# if __name__ == "__main__":
-#     parser = argparse.ArgumentParser(description="Welcome to web scraper")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Welcome to web scraper")
+    parser.add_argument("p", dest="book_price", choices=book_dict, nargs="+", help="Book prices")
 #     parser.add_argument("-b", dest="genres", choices=GENRES, nargs="+", help="List of genres to search through")
 #     parser.add_argument("size", type=str, choices=SIZES.keys(), help='Size of your shirt')
 #     parser.add_argument('color', type=str, choices=COLORS.keys(), help="Color or your shirt")
