bbb5dfd69c4ee1ea8883a9d7b0dcbff7f52c45dd
Georgi Kirilov <gkirilov1987@abv.bg>
2023-01-28 12:32:14 +0200

    add url

--- Homeworks/Tkinter/example.py
+++ Homeworks/Tkinter/example.py
@@ -13,6 +13,7 @@ label1 = Label(text='Test', font=("Arial", 24, "bold"))
 # place button where you want (two options - pack and place)
 label1.pack(side="bottom")
 # label1.place(x=50, y=0)
+# label1.grid(columnspan=3, rowspan=3, row=1, column=2)
 
 # two ways to config properties of label
 label1['text'] = 'BOOK'      # first way
@@ -57,5 +58,40 @@ scale1 = Scale(from_=0, to=100, command=scale_get)
 scale1.pack()
 
 
+def check_on_off():              # that returns 0 or 1 when click on check button
+    print(check_state.get())
+
+
+# create check button
+check_state = IntVar()
+check_button1 = Checkbutton(text='Turn ON/OFF', variable=check_state, command=check_on_off)
+check_state.get()
+check_button1.pack()
+
+def radio_used():
+    print(radio_state.get())
+# create radio button
+radio_state = IntVar()
+radio_button1 = Radiobutton(text='Opt1', value=1, variable=radio_state, command=radio_used)
+radio_button2 = Radiobutton(text='Opt2', value=2, variable=radio_state, command=radio_used)
+radio_button1.pack()
+radio_button2.pack()
+
+def use_listbox(event):
+    print(listbox.get(listbox.curselection()))
+
+listbox = Listbox(height=3)
+options1 = ['author', 'pages', 'title']
+for option in options1:
+    listbox.insert(options1.index(option), option)
+
+listbox.bind("<<ListBoxSelect>>", use_listbox)
+listbox.pack()
 window.mainloop()
 #end
+
+
+
+# за бутона има state - NORMAL, DISABLED.
+# може във функцията press_button да сложим button['state'] =
+# DISABLED и така става неактивен
