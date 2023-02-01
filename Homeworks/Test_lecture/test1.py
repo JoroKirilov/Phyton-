0a35454619b9f6295a7a1105ab24945ca3e26503
Georgi Kirilov <gkirilov1987@abv.bg>
2023-02-01 08:00:16 +0200

    exercises

---
+++ Homeworks/Test_lecture/test1.py
@@ -0,0 +1,27 @@
+num = 10
+num1 = 20
+srt1 = "nn"
+list1 = [i for i in range(1, 20)]
+# print(list1)
+# for i in list1:
+#     print(i)
+# else:
+#     print("no")
+
+try:
+    p = num - num1
+    print(p)
+except Exception:
+    print("fs")
+else:
+    print("fa")
+finally:
+    print("every time")
+# from unicodedata import decimal
+
+# # dec= {}
+# # dec[1] = 2
+# # dec[1] = 4
+# # print(dec)
+# myset = {"apple", "banana", "cherry"}
+# print(len(myset))
