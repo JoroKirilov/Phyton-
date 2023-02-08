0be40fb6bcf847789122d175aab7e0f5fb03d4d6
Georgi Kirilov <gkirilov1987@abv.bg>
2023-02-08 10:06:40 +0200

    second unit test

--- Exercises/testing/test1.py
+++ Exercises/testing/test1.py
@@ -1,7 +1,18 @@
 import unittest
 
+
 class SimpleTest(unittest.TestCase):
     def test_upper(self):
         input_str = 'foo'.upper()
-        result = 'FOo'
+        result = 'FOO'
         self.assertEqual(input_str, result, "This is simple test")
+
+    def test_add_operation(self):
+        a = 10
+        b = 15
+        exp_result = 25
+        self.assertEqual(a + b, exp_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
