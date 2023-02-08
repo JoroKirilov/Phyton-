c40d1aa45a3c1eea76c7a8e91ccb2662580d7764
Georgi Kirilov <gkirilov1987@abv.bg>
2023-02-08 12:24:11 +0200

    exception test

--- Exercises/testing/exceptions_test.py
+++ Exercises/testing/exceptions_test.py
@@ -18,5 +18,6 @@ class Worker:
 class TestWorkerClass(unittest.TestCase):
     def test_if_worker_not_energy_raise_exception(self):
         worker1 = Worker("John", 0)
-        with self.assertRaises(Exception) as ex:
+        with self.assertRaises(Exception) as ex:   # when know that we have exception, find if the exception raise
             worker1.work()
+        self.assertEqual("NOT ENOUGH ENERGY", str(ex.exception)) # and check exception msg
