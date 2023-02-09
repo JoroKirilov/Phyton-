62fdd757a38ebad55c9bfceb7f8033c98048494f
Georgi Kirilov <gkirilov1987@abv.bg>
2023-02-09 10:20:21 +0200

    subtest module test

---
+++ Exercises/testing/subtesting_module.py
@@ -0,0 +1,18 @@
+from decimal import Decimal
+from unittest import TestCase
+
+def sum_two_numbers(a, b):
+    return a + b
+
+class SimpleFunction(TestCase):
+    def test_sum_two_numbers(self):
+        test_cases = [
+            ((4, 5), 9),
+            (('4', 5), 9),
+            ((None, 5), 9),
+            ((Decimal(5), 5.1), 10.1)
+        ]
+
+        for (a, b), result in test_cases:
+            with self.subTest(result=result):
+                self.assertEqual(sum_two_numbers(a, b), result)
