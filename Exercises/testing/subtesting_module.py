0223d86abb91c3d9edb5c433242ac258936c0157
Georgi Kirilov <gkirilov1987@abv.bg>
2023-02-09 10:33:51 +0200

    subtest module test

--- Exercises/testing/subtesting_module.py
+++ Exercises/testing/subtesting_module.py
@@ -8,11 +8,14 @@ class SimpleFunction(TestCase):
     def test_sum_two_numbers(self):
         test_cases = [
             ((4, 5), 9),
-            (('4', 5), 9),
-            ((None, 5), 9),
-            ((Decimal(5), 5.1), 10.1)
+            # (('4', 5), 9),
+            # ((None, 5), 9),
+            # ((Decimal(5), 5.1), 10.1)
         ]
 
         for (a, b), result in test_cases:
             with self.subTest(result=result):
                 self.assertEqual(sum_two_numbers(a, b), result)
+
+        self.assertRaises(TypeError, lambda: sum_two_numbers('4', 5))
+        self.assertRaises(TypeError, lambda: sum_two_numbers(None, 6))
