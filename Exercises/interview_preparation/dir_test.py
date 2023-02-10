ba8587d6c84565a2e7815bb24581ece6c95ee84a
Georgi Kirilov <gkirilov1987@abv.bg>
2023-02-10 09:08:46 +0200

    list training

---
+++ Exercises/interview_preparation/dir_test.py
@@ -0,0 +1,13 @@
+import math
+
+print(dir(math))
+
+list1 = [1, 2, 3, [1, 2]]
+print(dir(list1))
+# list1.clear()
+print(list1)
+list1.reverse()
+print(list1)
+print(list1.count(68))
+print(list1.index(2))
+print(list1.index(2, 1, 3))
