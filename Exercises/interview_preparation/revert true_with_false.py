36bbafb4838746034f7f0afddabef7434271295d
Georgi Kirilov <gkirilov1987@abv.bg>
2023-02-12 15:57:01 +0200

    list training

---
+++ Exercises/interview_preparation/revert true_with_false.py
@@ -0,0 +1,9 @@
+def tab_turn_on_button(turn_on_off):
+    return not turn_on_off
+
+
+button = False
+button = tab_turn_on_button(button)
+print(button)
+button = tab_turn_on_button(button)
+print(button)
