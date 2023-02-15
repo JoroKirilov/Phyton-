1029b715e4990da29fa761d2aef9eb6336faac08
Georgi Kirilov <gkirilov1987@abv.bg>
2023-02-15 14:28:03 +0200

    decorators

--- Exercises/interview_preparation/decorators.py
+++ Exercises/interview_preparation/decorators.py
@@ -1,3 +1,5 @@
+# ------------ decorator and function that don't get arguments -----------
+
 # def get_volew_filter(function):
 #     def wrapper():
 #         letter = function()
@@ -10,7 +12,35 @@
 #
 # print(get_letter())
 
-def repeat(n):
-    def decorator(fucntion):
-        def wrapper():
 
+# ----------- decorators that get arguments ---------------
+
+# def repeat(n):
+#     def decorator(fucntion):
+#         def wrapper():
+#             tmp_str = ''
+#             for _ in range(n):
+#                 tmp_str += fucntion()
+#                 tmp_str += '\n'
+#             return tmp_str
+#         return wrapper
+#     return decorator
+#
+# @repeat(10)
+# def say_hi():
+#     return 'Hello'
+#
+# print(say_hi())
+
+# --------------------- function that get arguments -------------------------
+# def even_number(function):
+#     def wrapper(numbers):
+#         res = [el for el in numbers if el % 2 == 0]
+#         return res
+#     return wrapper
+#
+# @even_number
+# def get_numbers(numbers):
+#     return numbers
+#
+# print(get_numbers([1, 2, 3, 4]))
