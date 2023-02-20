1a41ba7d9528373cf8b0fdc25dd4a99c80209565
Georgi Kirilov <gkirilov1987@abv.bg>
2023-02-20 22:00:30 +0200

    training

--- Exercises/interview_preparation/web_scraper_exam.py
+++ Exercises/interview_preparation/web_scraper_exam.py
@@ -1,13 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
+#
+# req = requests.get("https://books.toscrape.com/index.html")
+# soup = BeautifulSoup(req.content, 'html.parser')
+# category_sidebar = soup.find(class_='nav nav-list')
+# category_list = category_sidebar.find_all("a")
+# category_dict = {}
+# for el in category_list:
+#     url = "http://books.toscrape.com/" + el["href"]
+#     category_dict[el.text.strip()] = url
+#
+# print(category_dict)
 
-req = requests.get("https://books.toscrape.com/index.html")
-soup = BeautifulSoup(req.content, 'html.parser')
-category_sidebar = soup.find(class_='nav nav-list')
-category_list = category_sidebar.find_all("a")
-category_dict = {}
-for el in category_list:
-    url = "http://books.toscrape.com/" + el["href"]
-    category_dict[el.text.strip()] = url
+# url = 'http://books.toscrape.com/catalogue/category/books/travel_2/'
+# url = "/".join(url.split("/")[0:-3:1])
+# print(url)
+# # base_url = "/".join(url.split("/")[0:-3:1])
+# # print(base_url)
+import requests
 
-print(category_dict)
+req = requests.get("http://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html")
+soup = BeautifulSoup(req.content, "html.parser")
+books = soup.find_all(class_='col-xs-6 col-sm-4 col-md-3 col-lg-3')
+for book in books:
+    print(book)
