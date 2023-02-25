02ff188b04fe0dd7b823c314d4afe56e13582db5
Georgi Kirilov <gkirilov1987@abv.bg>
2023-02-25 10:46:15 +0200

    itertools rulez

--- Exercises/interview_preparation/itertoos.py
+++ Exercises/interview_preparation/itertoos.py
@@ -3,9 +3,9 @@ lst = [12, 3, 1, 2, -6, 5, -8, 6]
 target = 0
 result_list = []
 resutl = list(itertools.combinations(lst, 3))
-print(resutl)
+
 for el in resutl:
-    if sum(el) == target and el not in result_list:
+    if sum(el) == target:
         result_list.append(el)
 
 print(result_list)
