702a81e4d614798371092394ef60a4df0a0f8991
Georgi Kirilov <gkirilov1987@abv.bg>
2023-02-25 13:12:13 +0200

    itertool cycle

--- Exercises/interview_preparation/revert true_with_false.py
+++ Exercises/interview_preparation/revert true_with_false.py
@@ -1,9 +1,17 @@
-def tab_turn_on_button(turn_on_off):
-    return not turn_on_off
+# def tab_turn_on_button(turn_on_off):
+#     return not turn_on_off
+#
+#
+# button = False
+# button = tab_turn_on_button(button)
+# print(button)
+# button = tab_turn_on_button(button)
+# print(button)
 
 
-button = False
-button = tab_turn_on_button(button)
-print(button)
-button = tab_turn_on_button(button)
-print(button)
+from itertools import cycle
+
+button_mode = cycle(("On", "Off"))
+print(next(button_mode))
+print(next(button_mode))
+print(next(button_mode))
