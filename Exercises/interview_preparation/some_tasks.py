4dc6db5ee1f6bc33624dcc3fe56bc08e666bb695
Georgi Kirilov <gkirilov1987@abv.bg>
2023-03-04 14:34:07 +0200

    found python dzen

--- Exercises/interview_preparation/some_tasks.py
+++ Exercises/interview_preparation/some_tasks.py
@@ -3,21 +3,61 @@
 # is_good = "good" if food == "apple" else "no good"
 # print(is_good)
 import itertools
-import math
-import operator
-from functools import reduce
-from itertools import accumulate
+# import math
+# import operator
+# from functools import reduce
+# from itertools import accumulate
+#
+# n = 3
+# print(math.factorial(n))
+# result = accumulate(range(1, n + 1), operator.mul)
+# print(list(result)[-1])
+#
+# result1 = 1
+# for i in range(1, n + 1):
+#     result1 = i * result1
+# print(result1)
+#
+# result_factoriel = reduce(operator.mul, range(1, n + 1))
+# print(result_factoriel)
+#
+#
+# def add_list(arr):
+#     tmp_list = arr.copy()
+#     tmp_list.append([1, 2, 3, 4, 5, 6])
+#     return tmp_list
+#
+# list_of_numbers = [1, 2, 3, 4, 5, 6]
+#
+# lis = add_list(list_of_numbers)
+# print(list_of_numbers)
+# print(lis)
 
-n = 3
-print(math.factorial(n))
-result = accumulate(range(1, n + 1), operator.mul)
-print(list(result)[-1])
+# def change_name():
+#     global name
+#     name = "Ivan"
+#
+# def new_name():
+#     global name
+#     name = "dragan"
+#
+#
+# name = ""
+# change_name()
+# new_name()
+# print(name)
+#
+# def koko(list_1):
+#     list_1["one"] = 1
+#
+#
+# a = {
+#     "five": 5,
+#     "six": 6
+#      }
+# koko(a)
+# print(a)
 
-result1 = 1
-for i in range(1, n + 1):
-    result1 = i * result1
-print(result1)
+import this
 
-result_factoriel = reduce(operator.mul, range(1, n + 1))
-print(result_factoriel)
-
+print(this)
