98d56e8a24f5bef05b003775cd160800b7698d12
Georgi Kirilov <gkirilov1987@abv.bg>
2023-03-05 13:17:06 +0200

    add

--- Exercises/interview_preparation/file_exercises.py
+++ Exercises/interview_preparation/file_exercises.py
@@ -1,11 +1,11 @@
 import os
 
-if os.path.isfile("file1.txt"):
-    print("file is already exist")
-else:
-    with open("file1.txt", "w") as file:
-        file.write("bomba")
+def write_more():
+    with open("file1.txt", "a") as f:
+        f.write("more text")
 
+with open("file1.txt", "w") as file:
+    file.write("bomba")
 
-
+write_more()
 
