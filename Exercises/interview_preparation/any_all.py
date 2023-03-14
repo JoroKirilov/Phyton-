4db329d7cfd3cbca4fab3b715c0ae4fa68288561
Georgi Kirilov <gkirilov1987@abv.bg>
2023-03-14 11:25:56 +0200

    add

--- Exercises/interview_preparation/any_all.py
+++ Exercises/interview_preparation/any_all.py
@@ -1,4 +1,4 @@
-str1 = "Aafsaasa"
+str1 = "aafsaasa"
 # count = 0
 # for char in str1:
 #     if char.isupper():
@@ -10,7 +10,14 @@ str1 = "Aafsaasa"
 # for char in str1:
 #     print(char.upper(), end="")
 
-is_have_upper = all(True if char.islower() else False for char in str1)
+# is_have_upper = all(True if char.islower() else False for char in str1)
+# print(is_have_upper)
+# is_have_upper = any(True if char.isupper() else False for char in str1)
+# print(is_have_upper)
+
+
+
+is_have_upper = [True if char.isupper() else False for char in str1]
 print(is_have_upper)
-is_have_upper = any(True if char.isupper() else False for char in str1)
-print(is_have_upper)
+is_any_upper_is_string = [True for char in str1 if char.isupper()]
+print(is_any_upper_is_string)
